5c871f8f7f0fb775738267a3fd9b5a256edc43ac
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2023-07-18 19:42:59 +0900

    Updated the conf file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,9 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+# -- Options for hoverxref.extension https://sphinx-hoverxref.readthedocs.io/en/latest/configuration.html
+hoverxref_auto_ref = True
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
